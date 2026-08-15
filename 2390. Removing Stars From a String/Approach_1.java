a3901d9d492ae02247e871f2ea7e0807b8fedaf4
Vansh Rai Chaudhary <177948862+VanshRai7000@users.noreply.github.com>
2026-08-15 12:53:06 +0530

    Medium: 2390. Removing Stars From a String | Approach 1 | Runtime: 530 | Memory: 52476000

---
+++ 2390. Removing Stars From a String/Approach_1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character>st = new Stack<>();
+        for(int  i = 0 ;i < s.length(); i++){
+            if(s.charAt(i) != '*'){
+                st.push(s.charAt(i));
+            }else{
+                st.pop();
+            }
+        }
+
+        String ans = "";
+
+        if(st.empty()){
+            return ans;
+        }
+        while(!st.empty()){
+            ans += st.peek();
+            st.pop();
+        }
+
+        return new StringBuilder(ans).reverse().toString();
+    }
+}
